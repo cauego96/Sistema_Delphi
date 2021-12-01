598a7e751890581fd43128296d5d45f6e9aa01c7
Caue Oliveira <cauego96@hotmail.com>
2021-12-01 17:48:51 -0300

    Correções para conexão ao banco de dados

--- Forms/uFormMain.dfm
+++ Forms/uFormMain.dfm
@@ -13,4 +13,13 @@ object FormMain: TFormMain
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 176
+    Top = 80
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
 end
--- prjCursoBasico.dpr
+++ prjCursoBasico.dpr
@@ -2,13 +2,18 @@ program prjCursoBasico;
 
 uses
   Vcl.Forms,
-  uFormMain in 'Forms\uFormMain.pas' {FormMain};
+  uFormMain in 'Forms\uFormMain.pas' {FormMain},
+  uDmDados in 'Forms\uDmDados.pas' {dmDados: TDataModule},
+  uBiblioteca in 'Classes\uBiblioteca.pas',
+  uFormConfigBanco in 'Forms\uFormConfigBanco.pas' {FormConfigBanco};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TdmDados, dmDados);
   Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TFormConfigBanco, FormConfigBanco);
   Application.Run;
 end.
